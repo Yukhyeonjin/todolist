fa66772a2ac29296ba0004fe040ddb619cc6876b
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-05-13 23:54:45 +0900

    [2021-05-13] todolist 초안 작업

---
+++ todolist/src/main/java/com/yuk/todolist/TodolistApplication.java
@@ -0,0 +1,14 @@
+package com.yuk.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+		System.out.println("드디어 되는건가..");
+	}
+
+}
